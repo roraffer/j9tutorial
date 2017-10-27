a61a4f20c4a3adcd957cd998b2ee71508d03c8b3
roraffer <ronanorafferty@yahoo.ie>
2017-10-27 12:14:38 +0100

    Reformat

--- first-module/src/com.ror.hello/com/ror/hello/MyMain.java
+++ first-module/src/com.ror.hello/com/ror/hello/MyMain.java
@@ -3,9 +3,9 @@ package com.ror.hello;
 
 public class MyMain {
 
-public static void main(String[] args) {
-    System.out.println("");
-    System.out.println("Hello, World from my first module");
-    System.out.println("");
-}
+    public static void main(String[] args) {
+        System.out.println("");
+        System.out.println("Hello, World from my first module");
+        System.out.println("");
+    }
 }
